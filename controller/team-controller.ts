02c6faa3dd73a3d3b6992ad77ea87970c079e7a0
Damian <dkowalski.1997@gmail.com>
2024-06-19 22:21:34 +0200

    added query endpoint for teams

--- controller/team-controller.ts
+++ controller/team-controller.ts
@@ -1,9 +1,17 @@
+import { Op } from 'sequelize'
 import Team from '../db/models/team.model'
 
 /**
+ * Queries teams based on the search query and the maximum
+ * size.
  *
  * @returns
  */
-export const queryTeams = async (): Promise<Team[]> => {
-    return []
+export const queryTeams = async (
+    size: number,
+    search: string
+): Promise<Team[]> => {
+    return await Team.findAll({
+        where: { name: { [Op.substring]: search } },
+    })
 }
--- routes/team-route.ts
+++ routes/team-route.ts
@@ -1,13 +1,24 @@
 import express, { NextFunction, Request, Response } from 'express'
 import * as teamController from '../controller/team-controller'
+import { InvalidPayloadError } from '../error/invalid-payload'
 
 const router = express.Router()
 
 router.get(
-    '/query',
+    '/search',
     async (req: Request, res: Response, next: NextFunction) => {
         try {
-            return await teamController.queryTeams()
+            const { size, search } = req.query
+            if (!size || !search) {
+                throw new InvalidPayloadError('Size or search not specified')
+            }
+
+            if (typeof size !== 'string' || typeof search !== 'string') {
+                throw new InvalidPayloadError('Invalid type of search or size')
+            }
+
+            const teams = await teamController.queryTeams(Number(size), search)
+            return res.json(teams)
         } catch (err) {
             return next(err)
         }
