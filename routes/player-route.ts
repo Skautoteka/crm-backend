5cf40e8c97289ceae86f0bd130b83fa0de3f4dbf
Damian <dkowalski.1997@gmail.com>
2024-08-21 22:27:28 +0200

    player route

--- routes/player-route.ts
+++ routes/player-route.ts
@@ -1,14 +1,14 @@
-import express, { NextFunction, Request, Response } from 'express';
-import * as playerController from '../controller/player-controller';
+import express, { NextFunction, Request, Response } from 'express'
+import * as playerController from '../controller/player-controller'
 
 const router = express.Router()
 
 router.get('/all', async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const players = await playerController.getAll();
+        const players = await playerController.getAll()
         return res.json(players)
     } catch (err) {
-        return next(err);
+        return next(err)
     }
 })
 
@@ -44,3 +44,5 @@ router.get(
         }
     }
 )
+
+export { router as playerRouter }
