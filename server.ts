7b82a5b768403feae81418c0231febbb73ba67c1
Damian Kowalski <dkowalski.1997@gmail.com>
2024-10-01 21:46:15 +0200

    removed useless route

--- server.ts
+++ server.ts
@@ -6,7 +6,6 @@ import { json } from 'body-parser'
 import { sequelize } from './db/sequelize'
 import { errorHandler } from './middleware/error-middleware'
 
-import { userRouter } from './routes/user-route'
 import { taskRouter } from './routes/task-route'
 import { reportRouter } from './routes/report-route'
 import { teamRouter } from './routes/team-route'
@@ -19,7 +18,6 @@ const app = express()
 app.use(json({ limit: '5mb' }))
 app.use(cors())
 
-app.use('/api/user', userRouter)
 app.use('/api/task', taskRouter)
 app.use('/api/report', reportRouter)
 app.use('/api/team', teamRouter)
