17574c38bdb2e72609284917eacccd8ddb9b7db7
Jakub-Prus <jakub.prus03@gmail.com>
2024-11-01 23:52:13 +0100

    user route

---
+++ routes/user-route.ts
@@ -0,0 +1,15 @@
+import express, { NextFunction, Request, Response } from 'express'
+import * as userController from '../controller/user-controller'
+
+const router = express.Router()
+
+router.get('/all', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const users = await userController.getAll()
+        return res.json(users)
+    } catch (err) {
+        return next(err)
+    }
+})
+
+export { router as userRouter }
