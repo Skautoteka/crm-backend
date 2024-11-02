b2a134c5f312438bd0abe0c679ab32f0e0d948ac
Jakub-Prus <jakub.prus03@gmail.com>
2024-11-02 09:36:19 +0100

    removed alter: true as it was duplicating constraints

--- server.ts
+++ server.ts
@@ -31,7 +31,7 @@ app.use('/api/auth', authRouter)
 
 const port = process.env.PORT
 ;(async () => {
-    await sequelize.sync({ alter: true })
+    await sequelize.sync()
 
     app.listen(port, () => {
         console.log(`Listening on port: ${port}`)
