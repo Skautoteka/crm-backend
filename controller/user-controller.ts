1a1f54d0bbf7ad41d7635a07b5292cbf07a13fe6
Jakub-Prus <jakub.prus03@gmail.com>
2024-11-02 22:44:02 +0100

    user create fields

--- controller/user-controller.ts
+++ controller/user-controller.ts
@@ -1,5 +1,6 @@
 import User from '../db/models/user.model'
 import Role from '../db/models/role.model'
+import { ISingleInputConfig } from '../interface'
 
 export const getAll = async () => {
     const users = await User.findAll({
@@ -15,4 +16,55 @@ export const getAll = async () => {
         ...user.get(),
         role: user.role?.name,
     }))
+}
+
+/**
+ * Gets the model for the user model creation.
+ *
+ * @returns
+ */
+export const getUserCreateFields = async (): Promise<ISingleInputConfig[]> => {
+    return [
+        {
+            name: 'firstName',
+            label: 'Imię',
+            isRequired: true,
+            placeholder: 'Wpisz imię',
+            type: 'TEXT',
+        },
+        {
+            name: 'lastName',
+            label: 'Nazwisko',
+            isRequired: true,
+            placeholder: 'Wpisz nazwisko',
+            type: 'TEXT',
+        },
+        {
+            name: 'email',
+            label: 'Adres email',
+            isRequired: true,
+            placeholder: 'Wpisz adres email',
+            type: 'TEXT',
+        },
+        {
+            name: 'rola',
+            label: 'Rola',
+            isRequired: true,
+            placeholder: 'Wybierz role użytkownika',
+            type: 'SELECT',
+            options: [
+                { label: 'Skaut', value: 'scout' },
+                { label: 'Menadżer regionu', value: 'manager' },
+                { label: 'Analityk', value: 'analyst' },
+            ],
+        },
+        {
+            name: 'region',
+            label: 'Region',
+            isRequired: false,
+            placeholder: 'Wyszukaj region',
+            type: 'SELECT',
+            searchType: 'region',
+        },
+    ]
 }
