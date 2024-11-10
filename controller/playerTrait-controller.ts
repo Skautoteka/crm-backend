9be219d760affb6eb601db739361a101ada4ea01
DominikRPaczkowski <153921387+DominikRPaczkowski@users.noreply.github.com>
2024-11-10 21:15:08 +0100

    Dodanie modelu dla cech piłkarzy (do raportów i analiz). (#8)
    * Dodanie modelu dla cech piłkarzy (do raportów i analiz).
    
    * Update 20241105212704-playerTraits.js
    
    Zmiana ID na UUID
    
    * Update playerTrait-controller.ts
    
    Removing unused references.
    
    * Update player.model.ts
    
    'NotNull' is defined but never used

---
+++ controller/playerTrait-controller.ts
@@ -0,0 +1,63 @@
+import PlayerTrait, { PlayerTraitCreationAttributes } from "../db/models/playerTrait.model";
+import { ModelValidationError } from "../error/model-validation";
+import { NotFoundError } from "../error/not-found";
+
+/**
+ * Returns all traits.
+ */
+export const getAllTraits = async (): Promise<PlayerTrait[]> => {
+    return await PlayerTrait.findAll();
+};
+
+/**
+ * Adds a new trait.
+ *
+ * @param payload
+ * @returns
+ */
+export const addTrait = async (payload: PlayerTraitCreationAttributes): Promise<PlayerTrait> => {
+    try {
+        const trait = await new PlayerTrait(payload).save();
+        const addedTrait = await PlayerTrait.findByPk(trait.id);
+
+        if (!addedTrait) {
+            throw new NotFoundError("Could not find added player trait.");
+        }
+
+        return addedTrait;
+    } catch (err) {
+        throw new ModelValidationError(err.message);
+    }
+};
+
+/**
+ * Removes trait.
+ *
+ * @param id
+ */
+export const removeTrait = async (id: string): Promise<void> => {
+    const trait = await PlayerTrait.findOne({ where: { id } });
+
+    if (trait) {
+        await trait.destroy();
+    } else {
+        throw new NotFoundError("Player trait not found.");
+    }
+};
+
+/**
+ *  Gets the model.
+ *
+ * @returns
+ */
+export const getTraitCreateFields = async () => {
+    return [
+        {
+            name: "name",
+            label: "Nazwa cechy",
+            isRequired: true,
+            placeholder: "Wpisz nazwę cechy",
+            type: "TEXT",
+        },
+    ];
+};
