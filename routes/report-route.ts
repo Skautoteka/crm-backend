ca78a58f2260453283bd8e15f7218223222c6ad3
Damian Kowalski <dkowalski.1997@gmail.com>
2024-11-13 23:13:09 +0100

    added report guards

--- routes/report-route.ts
+++ routes/report-route.ts
@@ -2,7 +2,7 @@ import express, { NextFunction, Request, Response } from 'express'
 import * as reportController from '../controller/report-controller'
 import * as authController from '../controller/auth-controller';
 import { routePermission } from '../permissions';
-import { MODULE_PERMISSIONS } from '../permissions/report';
+import { CREATE_PERMISSIONS, MODULE_PERMISSIONS, READ_PERMISSIONS, REMOVE_PERMISSIONS } from '../permissions/report';
 
 const router = express.Router()
 
@@ -12,6 +12,7 @@ router.use(
 
 router.get(
     '/create-fields',
+    routePermission(CREATE_PERMISSIONS),
     async (req: Request, res: Response, next: NextFunction) => {
         try {
             const fields = await reportController.getReportCreateFields()
@@ -22,7 +23,7 @@ router.get(
     }
 )
 
-router.get('/all', async (req: Request, res: Response, next: NextFunction) => {
+router.get('/all', routePermission(READ_PERMISSIONS), async (req: Request, res: Response, next: NextFunction) => {
     try {
         const user = await authController.getReqUser(req)
         const reports = await reportController.getAll(user)
@@ -43,6 +44,7 @@ router.get('/all-detailed', async (req: Request, res: Response, next: NextFuncti
 
 router.delete(
     '/:id',
+    routePermission(REMOVE_PERMISSIONS),
     async (req: Request, res: Response, next: NextFunction) => {
         const { id } = req.params
         try {
@@ -54,7 +56,7 @@ router.delete(
     }
 )
 
-router.post('/', async (req: Request, res: Response, next: NextFunction) => {
+router.post('/', routePermission(CREATE_PERMISSIONS), async (req: Request, res: Response, next: NextFunction) => {
     try {
         const user = await authController.getReqUser(req)
         const team = await reportController.add(req.body, user)
