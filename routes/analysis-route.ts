be3190b02086ac1d18bc888111e8f1a57b127d23
Damian Kowalski <dkowalski.1997@gmail.com>
2025-01-09 19:07:01 +0100

    route fix

--- routes/analysis-route.ts
+++ routes/analysis-route.ts
@@ -3,22 +3,16 @@ import * as analysisController from '../controller/analysis-controller'
 
 const router = express.Router()
 
-router.get(
-    '/analysis/get-report-filters',
-    async (req: Request, res: Response) => {
-        const filters = await analysisController.getReportFilters()
+router.get('/get-report-filters', async (req: Request, res: Response) => {
+    const filters = await analysisController.getReportFilters()
 
-        res.json({ filters })
-    }
-)
+    res.json({ filters })
+})
 
-router.get(
-    '/analysis/get-note-filters',
-    async (req: Request, res: Response) => {
-        const filters = await analysisController.getNoteFilters()
+router.get('/get-note-filters', async (req: Request, res: Response) => {
+    const filters = await analysisController.getNoteFilters()
 
-        res.json({ filters })
-    }
-)
+    res.json({ filters })
+})
 
 export { router as analysisRouter }
